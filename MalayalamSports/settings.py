0e5ea2a471b9c3acd4ec0a8074500e14c8cafbf3
itisiyvinjose <itisiyvinjose@gmail.com>
2017-11-06 07:32:08 +0530

    modified file structure

--- MalayalamSports/settings.py
+++ MalayalamSports/settings.py
@@ -139,6 +139,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
     ),
