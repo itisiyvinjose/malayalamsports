393d0c82686600c722d6efa068c8c432389b3408
itisiyvinjose <itisiyvinjose@gmail.com>
2017-11-14 22:11:50 +0530

    cors

--- MalayalamSports/settings.py
+++ MalayalamSports/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o^e$4yqitmk#^t#5gxgvbl=gs6=@io)18x81i11)p^^*bn+w3y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CORS_ORIGIN_ALLOW_ALL = True
 
